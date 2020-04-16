e9e710dfab4ec515d626e08a2ad7628b55a8bd61
runqitian <tianrunqi94@gmail.com>
2020-04-16 16:12:56 -0400

    finish docker-compose.yml

--- webpage/static/precache-manifest.134e61d219b3ccac42fdb20afbc650e7.js
+++ webpage/static/precache-manifest.134e61d219b3ccac42fdb20afbc650e7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "23fc3da277234f440e5edf7d7975446d",
+    "revision": "3151671c312479bae3a198999d39a27b",
     "url": "/index.html"
   },
   {
-    "revision": "b9d4a11da33551dbabdd",
+    "revision": "990bea52bbb4dc84d72e",
     "url": "/static/css/2.7ececfe9.chunk.css"
   },
   {
-    "revision": "a5435db786fd5b8bee73",
+    "revision": "e4763e180f8f80359316",
     "url": "/static/css/main.d3bde9dd.chunk.css"
   },
   {
-    "revision": "b9d4a11da33551dbabdd",
-    "url": "/static/js/2.8de12c68.chunk.js"
+    "revision": "990bea52bbb4dc84d72e",
+    "url": "/static/js/2.bbec63ab.chunk.js"
   },
   {
-    "revision": "a5435db786fd5b8bee73",
-    "url": "/static/js/main.9c95f8fa.chunk.js"
+    "revision": "e4763e180f8f80359316",
+    "url": "/static/js/main.4f0a790b.chunk.js"
   },
   {
     "revision": "b1f2b9e144fd8aeb539b",
